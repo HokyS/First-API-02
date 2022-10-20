dec20b3cc7027ab57748cf12db9aedf41435afe0
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-10-20 04:42:07 +0200

    FirstApi 02 exercise

---
+++ src/main/java/com/example/FristApi02/FristApi02Application.java
@@ -0,0 +1,13 @@
+package com.example.FristApi02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristApi02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristApi02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FristApi02/FristApi02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FristApi02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristApi02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
